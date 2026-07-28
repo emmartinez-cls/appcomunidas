ae2b81afa15620a15ed7d42664846e8cb0bbc101
Eugenio Martinez <emmartinez@clsoluciones.cl>
2026-07-28 13:27:15 -0400

    Fix: Move index.html to src/ to prevent IIS from intercepting it at the root folder

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       '/api': {
