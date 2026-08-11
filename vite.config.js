e60bb6b39fa1e57de917d95bdc911795e05ec8f6
Eugenio Martinez <emmartinez@clsoluciones.cl>
2026-08-11 14:35:52 -0400

    Fix: Rename entry point to app.js for IISNode compatibility

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src',
-  publicDir: '../public',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
   server: {
     proxy: {
       '/api': {
